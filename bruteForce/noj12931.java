92d87d21cfd645fcd3304bd12c795684aa51e2f4
tmddus2 <tmddus2__@naver.com>
2023-05-13 17:34:43 -0700

    noj12931

---
+++ bruteForce/noj12931.java
@@ -0,0 +1,44 @@
+package bruteForce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class noj12931 {
+    public static int N;
+    public static int[] B;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        B = new int[N];
+        int sum = 0;
+        for(int i=0; i<N; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+            sum += B[i];
+        }
+
+        int ans = 0;
+        while(sum!=0) {
+            for (int i=0; i<N; i++) {
+                if (B[i]%2 == 1) {
+                    B[i] = B[i]-1;
+                    sum--;
+                    ans++;
+                }
+            }
+            if (sum == 0) {
+                continue;
+            }
+            for (int i=0; i<N; i++) {
+                B[i] = B[i]/2;
+            }
+            sum /= 2;
+            ans++;
+        }
+        System.out.println(ans);
+    }
+
+
+}
